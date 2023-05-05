2e37debeade63b2b58680faf4af325a09de9119f
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-05-05 16:30:56 -0700

    Start and stop server

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -1,8 +1,22 @@
 (ns backend.server
   (:require [org.httpkit.server :as kit]))
 
+(defonce server (atom nil))
+
 (defn handler [_]
   {:status 200
    :body "Hello from Clojure"})
 
-(kit/run-server handler {:port 5000})
+(defn start-server [port]
+  (reset! server (kit/run-server #'handler {:port port})))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(start-server 5000)
+
+(comment
+  (start-server 5001)
+  (stop-server))
