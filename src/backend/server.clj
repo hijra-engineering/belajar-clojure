05b01df7ec33f2fdc3e7aa477737d0c971b29aa6
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-05-05 16:35:11 -0700

    routing with Compojure

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -1,11 +1,19 @@
 (ns backend.server
-  (:require [org.httpkit.server :as kit]))
+  (:require [org.httpkit.server :as kit]
+            [compojure.core :refer [defroutes GET]]))
+
+(defn hello-world [_]
+  "Hallo world with Compojure")
+
+(defn make-greeter [name]
+  (fn [_] (str "Hi " name)))
+
+(defroutes handler
+  (GET "/" [] hello-world)
+  (GET "/hi" [] "Selamat pagi!")
+  (GET "/hi/:name" [name] (make-greeter name)))
 
 (defonce server (atom nil))
-
-(defn handler [_]
-  {:status 200
-   :body "Hello from Clojure"})
 
 (defn start-server [port]
   (reset! server (kit/run-server #'handler {:port port})))
@@ -19,4 +27,34 @@
 
 (comment
   (start-server 5001)
-  (stop-server))
+  (stop-server)
+
+  ;; simplest handler
+  (defroutes handler
+    (GET "/" [] "Hallo world with Compojure"))
+
+  ;; multiple paths
+  (defroutes handler
+    (GET "/" [] "Hallo world with Compojure")
+    (GET "/hi" [] "Hi there!"))
+
+  ;; use functions, anonymous and declared
+  (defn hi-there [_]
+    "Hi there!")
+  (defroutes handler
+    (GET "/" [] (fn [_] "Hello world"))
+    (GET "/hi" [] hi-there))
+
+  ;; higher-order
+  (defn make-salam []
+    (fn [_] "Salam"))
+  (defroutes handler
+    (GET "/" [] hello-world)
+    (GET "/hi" [] (make-salam)))
+
+  ;; URL parameter
+  (defn make-greeter [name]
+    (fn [_] (str "Hi " name)))
+  (defroutes handler
+    (GET "/" [] hello-world)
+    (GET "/hi/:name" [name] (make-greeter name))))
