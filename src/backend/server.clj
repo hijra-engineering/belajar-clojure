0200d541a30f19c55455fd601bf49f4e44ac5ac1
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-05-05 16:36:49 -0700

    wrap-reload for handler

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -1,60 +1,39 @@
 (ns backend.server
   (:require [org.httpkit.server :as kit]
+            [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]))
 
 (defn hello-world [_]
-  "Hallo world with Compojure")
+  "Hello world with Compojure")
 
 (defn make-greeter [name]
   (fn [_] (str "Hi " name)))
 
 (defroutes handler
   (GET "/" [] hello-world)
-  (GET "/hi" [] "Selamat pagi!")
   (GET "/hi/:name" [name] (make-greeter name)))
 
 (defonce server (atom nil))
 
 (defn start-server [port]
-  (reset! server (kit/run-server #'handler {:port port})))
+  (reset! server (kit/run-server (wrap-reload #'handler) {:port port})))
 
 (defn stop-server []
   (when-not (nil? @server)
     (@server :timeout 100)
     (reset! server nil)))
 
-(start-server 5000)
+(when (nil? @server)
+  (start-server 5000))
 
 (comment
-  (start-server 5001)
+  (start-server 5000)
   (stop-server)
 
-  ;; simplest handler
-  (defroutes handler
-    (GET "/" [] "Hallo world with Compojure"))
+  ;; when this replacement is used and evaluated,
+  ;; wrap-reload takes care of applying the new routes
 
-  ;; multiple paths
-  (defroutes handler
-    (GET "/" [] "Hallo world with Compojure")
-    (GET "/hi" [] "Hi there!"))
-
-  ;; use functions, anonymous and declared
-  (defn hi-there [_]
-    "Hi there!")
-  (defroutes handler
-    (GET "/" [] (fn [_] "Hello world"))
-    (GET "/hi" [] hi-there))
-
-  ;; higher-order
-  (defn make-salam []
-    (fn [_] "Salam"))
   (defroutes handler
     (GET "/" [] hello-world)
-    (GET "/hi" [] (make-salam)))
-
-  ;; URL parameter
-  (defn make-greeter [name]
-    (fn [_] (str "Hi " name)))
-  (defroutes handler
-    (GET "/" [] hello-world)
+    (GET "/salam" [] "Salam")
     (GET "/hi/:name" [name] (make-greeter name))))
